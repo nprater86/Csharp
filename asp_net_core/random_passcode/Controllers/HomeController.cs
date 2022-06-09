df355ad5735a6665c13ff6dc41551fd8b3377f5a
Nathan Prater <nprater86@gmail.com>
2022-06-09 12:48:56 -0500

    added random passcode

---
+++ asp_net_core/random_passcode/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using random_passcode.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace random_passcode.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        if(HttpContext.Session.GetInt32("counter") == null)
+        {
+            HttpContext.Session.SetInt32("counter", 0);
+        }
+        return View();
+    }
+    [HttpGet]
+    public IActionResult GeneratePasscode()
+    {
+        int counter = HttpContext.Session.GetInt32("counter") ?? default(int);
+        counter++;
+        HttpContext.Session.SetInt32("counter", counter);
+        Random rand = new Random();
+        List<string> characterList = new List<string>(){"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "1","2","3","4","5","6","7","8","9","0"};
+        string passcode = "";
+        for(int i = 0; i < 14; i++){
+            passcode += characterList[rand.Next(characterList.Count)];
+        }
+        Dictionary<string,object> data = new Dictionary<string,object>();
+        data.Add("Passcode", passcode);
+        data.Add("Counter",counter);
+        return Json(data);
+    }
+
+    public IActionResult ClearCounter(){
+        HttpContext.Session.SetInt32("counter", 1);
+        return Json(HttpContext.Session.GetInt32("counter"));
+    }
+}
---
+++ asp_net_core/random_passcode/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//set variables
+const passcodeDisplay = document.getElementById("passcodeDisplay");
+const generateBtn = document.getElementById("generateBtn");
+const clearBtn = document.getElementById("clearBtn");
+let counterDisplay = document.getElementById("counterDisplay");
+
+//create a function that fetches the passcode from the controller on the ASP .NET side
+async function getPasscode(){
+    const response = await fetch("http://localhost:5218/Home/GeneratePasscode");
+    const passcode = await response.json();
+    passcodeDisplay.innerHTML = passcode.Passcode;
+    counterDisplay.innerHTML = passcode.Counter;
+}
+
+//generate our first passcode
+getPasscode();
+
+//update passcode when button is clicked
+generateBtn.onclick = () => {
+    getPasscode();
+}
+
+clearBtn.onclick = async () => {
+    const response = await fetch("http://localhost:5218/Home/ClearCounter");
+    const counter = await response.json();
+    counterDisplay.innerHTML = counter;
+}
